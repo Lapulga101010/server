aeb89c5ba18085646a665c1ff03abf51cb861de3
Lapulga101010 <136895786+Lapulga101010@users.noreply.github.com>
2023-07-04 15:36:20 +0100

    Update index.js

--- index.js
+++ index.js
@@ -4,28 +4,31 @@ import cors from "cors";
 import cookieParser  from "cookie-parser";
 import jwt  from "jsonwebtoken";
 import multer from "multer";
-
+import { Server as SocketIOServer } from 'socket.io';
+import http from 'http';
 
 
 const app = express();
 app.use(cookieParser());
 app.use(express.json());
 
+const server = http.createServer(app);
+const io = new SocketIOServer(server);
 
 
 
 app.use(cors({
-    origin:"https://meta-groupe.net",
-    methods : ["POST, GET"],
-    credentials : true
+  origin:"https://meta-groupe.net",
+  methods : ["POST, GET"],
+  credentials : true
 }
 ));
 
 const db = mysql.createConnection({
-  host: "141.94.102.32",
-  user: "wgmnwtfv_meta-groupe",
-  password: "Metagroupe",
-  database: "wgmnwtfv_Meta-groupe",
+host: "141.94.102.32",
+user: "wgmnwtfv_meta-groupe",
+password: "Metagroupe",
+database: "wgmnwtfv_Meta-groupe",
 });
 const verifyUser = (req,res,next) =>{
     const token = req.cookies.token;
@@ -46,40 +49,14 @@ const verifyUser = (req,res,next) =>{
 }
 
 
+const upload = multer({ dest: 'upload/' }); // Indiquez le répertoire de destination pour stocker les fichiers
 
+app.post('/upload', upload.single('file'), (req, res) => {
+  // Le fichier est accessible via req.file
+  // Vous pouvez manipuler le fichier ici (par exemple, le déplacer vers un autre emplacement, le renommer, etc.)
 
-
-// Configuration de Multer pour spécifier le dossier de destination des fichiers téléchargés
-const storage = multer.diskStorage({
-    destination: function (req, file, cb) {
-      cb(null, 'uploads/');
-    },
-    filename: function (req, file, cb) {
-      const uniqueSuffix = Date.now() + '-' + Math.round(Math.random() * 1e9);
-      cb(null, uniqueSuffix + '-' + file.originalname);
-    },
-  });
-  
-  // Créez un objet de chargement Multer
-  const upload = multer({ storage: storage });
-  
-  // Endpoint pour gérer la demande de téléchargement de fichier
-  app.post('/upload', upload.single('file'), (req, res) => {
-    // Le fichier est accessible via req.file
-    if (req.file) {
-      // Fichier téléchargé avec succès
-      res.json({ message: 'Fichier téléchargé avec succès' });
-    } else {
-      // Aucun fichier n'a été envoyé ou une erreur s'est produite lors du téléchargement
-      res.status(400).json({ message: 'Erreur lors du téléchargement du fichier' });
-    }
-  });
-  
-
-
-
-
-
+  res.sendStatus(200); // Envoyez une réponse réussie au client
+});
 
 app.get('/',verifyUser,(req,res)=>{
 
@@ -125,8 +102,8 @@ app.get('/messages' ,(req,res)=>{
         }
     })
 const user = req.name;
-const groupe =req.body;
-    const sql="SELECT * FROM messages WHERE  username = ?";
+
+    const sql="SELECT * FROM messages INNER JOIN etab ON messages.usernameA = etab.CODETAB WHERE messages.username = ? order by date DESC ";
     db.query(sql,[user],(err,data)=>{
         if(err) return res.json(data);
 
@@ -147,13 +124,54 @@ app.get('/messagesS' ,(req,res)=>{
         }
     })
 const user = req.name;
-    const sql="SELECT * FROM messages WHERE usernameA = ?";
+    const sql="SELECT * FROM `messages` INNER JOIN etab ON messages.username = etab.CODETAB WHERE messages.usernameA = ? GROUP BY message  order by date DESC ";
     db.query(sql,[user],(err,data)=>{
         if(err) return res.json(data);
         return res.json(data)|| [];
         
     })
 })
+app.get('/luG' ,(req,res)=>{
+  const token = req.cookies.token;
+  jwt.verify(token,"metagroupe",(err , decoded)=>{
+      if(err){
+  return res.json({Message : "Auth err"});
+      }
+      else{
+          req.name = decoded.name;
+         
+      }
+  })
+const user = req.name;
+const message= req.query.id_m;
+console.log();
+  const sql="SELECT * FROM `messages` INNER JOIN etab ON messages.username = etab.CODETAB where messages.id_m= ? GROUP BY message ";
+  db.query(sql,[message],(err,data)=>{
+      if(err) return res.json(data);
+      return res.json(data)|| [];
+      
+  })
+})
+app.get('/gr' ,(req,res)=>{
+  const token = req.cookies.token;
+  jwt.verify(token,"metagroupe",(err , decoded)=>{
+      if(err){
+  return res.json({Message : "Auth err"});
+      }
+      else{
+          req.name = decoded.name;
+         
+      }
+  })
+const user = req.name;
+  const sql="SELECT * FROM user INNER JOIN groupes ON user.groupe = groupes.id WHERE user.username = ?";
+  db.query(sql,[user],(err,data)=>{
+      if(err) return res.json(data);
+      return res.json(data)|| [];
+      
+  })
+})
+
 
 
 app.get('/mes/:id_m' ,(req,res)=>{
@@ -171,7 +189,7 @@ app.get('/mes/:id_m' ,(req,res)=>{
 
 const user = req.name;
 
-    const sql="SELECT * FROM messages WHERE id_m = ?";
+    const sql="SELECT * FROM messages INNER JOIN etab ON messages.usernameA = etab.CODETAB WHERE id_m = ?";
 
     db.query(sql,[userId],(err,data)=>{
         if(err) return res.json(data);
@@ -244,6 +262,40 @@ app.post('/val/:id_m', (req, res) => {
 });
 
 
+
+app.post('/lu/:id_m', (req, res) => {
+  const token = req.cookies.token;
+  jwt.verify(token, "metagroupe", (err, decoded) => {
+    if (err) {
+      return res.json({ Message: "Auth err" });
+    } else {
+      req.name = decoded.name;
+    }
+  });
+  
+
+  const userId = req.params.id_m;
+  const user = req.name;
+
+  const sql = "UPDATE `messages` SET `lu`='1' WHERE id_m= ? ";
+
+  db.query(sql, [userId], (error, results) => {
+    if (error) {
+      console.error('Une erreur s\'est produite lors de l\'insertion des données:', error);
+      res.sendStatus(500);
+    } else {
+      console.log('Données insérées avec succès.');
+      res.sendStatus(200);
+    }
+  });
+});
+
+
+
+
+
+
+
 app.get('/user',(req,res)=>{
     const token = req.cookies.token;
     jwt.verify(token,"metagroupe",(err , decoded)=>{
@@ -257,7 +309,7 @@ app.get('/user',(req,res)=>{
     })
 
     const user = req.name;
-    const sql="SELECT id,username,name FROM user WHERE username !=?";
+    const sql="SELECT * FROM user INNER JOIN etab ON user.username = etab.CODETAB WHERE username !=?";
     db.query(sql,[user],(err,data)=>{
         if(err) return res.json(data);
         return res.json(data) || [];
@@ -267,57 +319,69 @@ app.get('/user',(req,res)=>{
 })
 app.post('/send', (req, res) => {
 
-    const token = req.cookies.token;
-    jwt.verify(token,"metagroupe",(err , decoded)=>{
-        if(err){
-    return res.json({Message : "Auth err"});
-        }
-        else{
-            req.name = decoded.name;
-           
-        }
-    })
-
-    const usertt = req.name;
-
-    const { groupes, users ,noms, value1, value2 } = req.body;
-    
-
-
-    const nn=noms.map((nom)=>nom.name);
-    // Insérer les données dans la base de données
-    const query = 'INSERT INTO messages (de,username,usernameA,nom, objet, message) VALUES ?';
-  const queryGroupe= 'INSERT INTO messages (de,usernameA,objet,message,groupe,nom) VALUES ?';
-    // Préparer les valeurs à insérer
-    const values = users.map((user) => [nn,user.username,usertt,user.name, value1, value2]);
-    const valuesG = groupes.map((groupe)=>[nn,usertt, value1, value2,groupe.id,groupe.nom_groupe]);
-    
-
-    if (groupes && groupes.length > 0) {
-        db.query(queryGroupe, [valuesG], (error, results) => {
-            if (error) {
-              console.error('Une erreur s\'est produite lors de l\'insertion des données:', error);
-              res.sendStatus(500);
-            } else {
-              console.log('Données insérées avec succès.');
-              res.sendStatus(200);
-            }
-          });
-      }else{
-        console.log('vide')
-      }
-      if (users && users.length > 0){
-    db.query(query, [values], (error, results) => {
-      if (error) {
-        console.error('Une erreur s\'est produite lors de l\'insertion des données:', error);
-        res.sendStatus(500);
+  const token = req.cookies.token;
+  jwt.verify(token, "metagroupe", (err, decoded) => {
+      if (err) {
+          return res.json({ Message: "Authentification erreur" });
       } else {
-        console.log('Données insérées avec succès.');
-        res.sendStatus(200);
+          req.name = decoded.name;
+
+          const usertt = req.name;
+
+          const { groupes, users, noms, value1, value2 } = req.body;
+
+          const nn = noms.map((nom) => nom.name);
+
+          // Insérer les données dans la base de données
+          const query = 'INSERT INTO messages (de, username, usernameA, nom, objet, message) VALUES ?';
+         
+
+          // Préparer les valeurs à insérer
+          const values = users.map((user) => [usertt, user.username, usertt, user.name, value1, value2]);
+          const queryGroupe = 'INSERT INTO messages (username, usernameA,  objet, message,groupe_m,groupe) SELECT username, ?, ?, ?, ? FROM `user` WHERE groupe = ?';
+          const queryGroupe2 = 'INSERT INTO groupe_m (messs) VALUES (?)';
+
+        
+          if (groupes && groupes.length > 0) {
+            db.query(queryGroupe2, usertt ,(error, results) => {
+              if (error) {
+                console.error('Une erreur s\'est produite lors de l\'insertion des données :', error);
+                res.sendStatus(500);
+              } else {
+                console.log('Données insérées avec succès.');
+                res.sendStatus(200);
+              }
+              const dernierId = results.insertId;
+              const valuesG = groupes.map((groupe) => [nn,usertt, value1, value2,dernierId, groupe.id,groupe.id]);
+              db.query(queryGroupe,valuesG.flat(), (error, results) => {
+                if (error) {
+                  console.error('Une erreur s\'est produite lors de l\'insertion des données :', error);
+                  res.sendStatus(500);
+                } else {
+                  console.log('Données insérées avec succès.');
+                  res.sendStatus(200);
+                }
+              });
+            });
+
+          }
+          
+          
+          if (users && users.length > 0) {
+              db.query(query, [values], (error, results) => {
+                  if (error) {
+                      console.error('Une erreur s\'est produite lors de l\'insertion des données :', error);
+                      res.sendStatus(500);
+                  } else {
+                      console.log('Données insérées avec succès.');
+                      res.sendStatus(200);
+                  }
+              });
+          }
       }
-    });
-}
   });
+});
+
   app.post('/form', (req, res) => {
     const token = req.cookies.token;
     jwt.verify(token,"metagroupe",(err , decoded)=>{
@@ -416,8 +480,7 @@ app.post('/send', (req, res) => {
 
   });
   
-
-app.post('/login',(req,res)=>{
+  app.post('/login',(req,res)=>{
 
     const sql="SELECT * FROM user WHERE username = ? AND password = ?";
     
@@ -457,7 +520,7 @@ app.get('/info_u',(req,res)=>{
     })
 
     const user = req.name;
-    const sql="SELECT * FROM user WHERE username =?";
+    const sql = "SELECT * FROM user INNER JOIN etab ON user.username = etab.CODETAB WHERE user.username = ?";
     db.query(sql,[user],(err,data)=>{
         if(err) return res.json(data);
         return res.json(data) || [];
@@ -489,15 +552,25 @@ app.get('/groupeL',(req,res)=>{
 })
 
 app.get('/logout',(req,res) => {
-  res.cookie('token', '', {
-    expires: new Date(0),
-    httpOnly: true,
-    secure: true,
-    sameSite: 'none'
-  });
+    res.clearCookie('token');
     return res.json({Status :"Success"});
 
 })
+
+
+
+io.on('connection', (socket) => {
+  console.log('Un client est connecté');
+
+  socket.on('disconnect', () => {
+    console.log('Un client est déconnecté');
+  });
+});
+
+
+
+
+
 app.listen(8081, ()=> {
-    console.log("Running 777887....");
+    console.log("Running on 8081 .....");
 })
